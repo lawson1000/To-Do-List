7013f9c333fb04b281583446a68de59b8a183b88
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-01-21 21:38:36 +0100

    add functionalities

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// alert("Hello World")
+window.addEventListener("load",() => {
+    const taskForm = document.querySelector("#task-form");
+    const newTask = document.querySelector("#new_task");
+    const listTask = document.querySelector("#tasks");
+
+    taskForm.addEventListener("submit",(e) =>{
+        // prevent it from refreshing the page
+        e.preventDefault();
+        
+        const taskValue = newTask.value;
+
+        if(!taskValue){
+            alert("Field cannot be empty")
+            return;
+        }
+        
+        const task_add = document.createElement("div")
+        task_add.classList.add("added-tasks")
+
+        const task_content_add = document.createElement("div");
+        task_content_add.classList.add("content");
+        task_content_add.innerText =taskValue;
+
+        task_add.appendChild(task_content_add);
+
+        listTask.appendChild(task_add);
+        
+    })
+})
